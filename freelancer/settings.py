ec5829bae15544114f33d13aea9bb826113c41f5
subha-py <subha4exam@gmail.com>
2016-09-08 20:51:38 +0530

    updating settings and adding requirements.txt

--- freelancer/settings.py
+++ freelancer/settings.py
@@ -108,5 +108,5 @@ STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='subha.py@gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_PASSWORD='mo8ivA94Tion'
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS=True
